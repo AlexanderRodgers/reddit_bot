4b3cd6476cdb02f8f16d399a133d937b11990fdf
Alex Rodgers <alex.e.rodgers@gmail.com>
2016-12-22 19:23:58 -0800

    Updated script
    updated the bot script.

--- new_bot_test.py
+++ new_bot_test.py
@@ -10,6 +10,7 @@ reddit = praw.Reddit(client_id='', #Program will not compile without filled in i
                      user_agent='',
                      username='')
 
+#Method to search for comments of a certain phrase, and then respond.
 def comment_post(comment_search, comment_reply):
     print(comment_search)
     print(comment_reply)
@@ -35,13 +36,16 @@ else:
         posts_replied_to = posts_replied_to.split('\n')
         posts_replied_to = list(filter(None, posts_replied_to))
 
-subreddit = reddit.subreddit('me_irlbot')
+subreddit = reddit.subreddit('') #add the name of the subreddit here.
+
+#subreddit.stream.comments() looks specifically at all comments in a subreddit.
 for comment in subreddit.stream.comments():
     if comment.id not in posts_replied_to:
         print('new comment in stream: ',comment.id)
+        comment_post('REPLACE HERE', 'REPLACE HERE') #replace with your own search for comment to reply to. Then replace with response.
         comment_post('REPLACE HERE', 'REPLACE HERE')
-        comment_post('REPLACE HERE', 'REPLACE HERE')
-
+        
+    #Save all comment id's to a txt file
     with open('posts_replied_to.txt', 'w') as f:
         for comment_id in posts_replied_to:
             f.write(comment_id + '\n')
